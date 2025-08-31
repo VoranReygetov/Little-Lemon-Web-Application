8ddb615fe8185a4d15ded508de54f0a694887dac
Reyget <voran009@gmail.com>
2025-08-31 19:57:34 +0300

    Removed __pycache__, added a page about API calls, added pulling static to a docker.

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -175,10 +175,6 @@ STATIC_URL = '/static/'  # URL path for static files
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will put them
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'restaurant' / 'static',  # optional extra static dirs
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('menu/', views.menu, name="menu"),
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
     path('bookings', views.bookings, name='bookings'), 
+    path("docs/", TemplateView.as_view(template_name="api_page.html"), name="api-docs"),
 ]
